19638468587bbb75e49d28e5396ab63e7388872f
İdil Tuğba Gölcük <idilgolcuk@gmail.com>
2025-10-01 05:40:09 +0300

    projects

--- components/portfolio/index.tsx
+++ components/portfolio/index.tsx
@@ -1,12 +1,21 @@
 "use client";
 
 function Portfolio() {
-    return (  
+  return (
     <>
-        <a href="https://www.canva.com/design/DAGx6o5T0Ko/bE_XK3i4oPYPzO4jNeTjQA/view?utm_content=DAGx6o5T0Ko&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h4f845ddabc"  target="_blank" rel="noopener">
-            <img src="/slider/portfolio/1.jpg" alt="Portfolio Image" className="mb-3 pr-4"/>
-        </a>
-    </>);
+      <a
+        href="https://www.canva.com/design/DAGx6o5T0Ko/bE_XK3i4oPYPzO4jNeTjQA/view"
+        target="_blank"
+        rel="noopener"
+      >
+        <img
+          src="/slider/portfolio/1.jpg"
+          alt="Portfolio Image"
+          className="mb-3 pr-4"
+        />
+      </a>
+    </>
+  );
 }
 
 export default Portfolio;
